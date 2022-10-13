d6fdba357e55067a3a8c4a99a6d71f2f2c8c14ab
benkoehlL <feynmandiagramateur@gmail.com>
2022-10-13 09:15:38 +0200

    safety commit

---
+++ adder.py
@@ -0,0 +1,204 @@
+'''
+This program sets up the Half Adder and the Full Adder and creates a .tex file
+with the gate geometry. It also evaluates the result with a qasm quantum 
+simulator
+'''
+
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, \
+                    execute, result, Aer
+import os
+import shutil
+
+LaTex_folder_Adder_gates = str(os.getcwd())+'/Latex_quantum_gates/Adder-gates/'
+if not os.path.exists(LaTex_folder_Adder_gates):
+    os.makedirs(LaTex_folder_Adder_gates)
+else:
+    shutil.rmtree(LaTex_folder_Adder_gates)
+    os.makedirs(LaTex_folder_Adder_gates)
+
+qubit_space = ['0','1']
+
+## define the previous logic gates
+def AND(qc, q0, q1, q2):
+    qc.reset(q2)
+    qc.ccx(q0,q1,q2)
+
+def OR(qc, q0, q1, q2):
+    qc.reset(q2)
+    qc.ccx(q0, q1, q2)
+    qc.cx(q0,q2)
+    qc.cx(q1,q2)
+
+def XOR(qc, q0, q1, q2):
+    qc.reset(q2)
+    qc.cx(q0,q2)
+    qc.cx(q1,q2)
+
+def NOR(qc, q0, q1, q2):
+    qc.reset(q2)
+    qc.ccx(q0, q1, q2)
+    qc.cx(q0,q2)
+    qc.cx(q1,q2)
+    qc.x(q2)
+
+def Add_with_carry(qc, q0, q1, c):
+    qc.ccx(q0, q1, c)
+    qc.cx (q0, q1)
+
+def Half_Adder(qc, q0,q1,q2,c):
+    # This function carries out the addition with a carry bit
+    # It also measures the minor bit in the addition
+    qc.ccx(q0,q1,q2)
+    qc.cx(q1, q0)
+    qc.measure(q0,c)
+
+## Half Adder (my version)
+# long version
+'''
+addend0 = '1' 
+addend1 = '1'
+q  = QuantumRegister(3, name = 'q')
+c  = ClassicalRegister(2, name = 'c')
+qc = QuantumCircuit(q,c)
+for qubit in q:
+    qc.reset(qubit)
+
+# initialisation
+if(addend0== '1'):
+    qc.x(q[0])
+if(addend1 == '1'):
+    qc.x(q[1])
+
+qc.ccx(q[0], q[1], q[2])
+qc.cx(q[0], q[1])
+qc.measure(q[1], c[0])
+qc.measure(q[2], c[1])
+
+backend = Aer.get_backend('qasm_simulator')
+job = execute(qc, backend, shots=1000)
+results = job.result()
+count = results.get_counts()
+#print(count)
+'''
+
+## Half Adder (my version)
+# short version
+print("Test Half Adder",'\n')
+
+for add0 in qubit_space: # loop over all possible additions
+    for add1 in qubit_space:            
+        q  = QuantumRegister(3, name = 'q')
+        c  = ClassicalRegister(2, name = 'c')
+        qc = QuantumCircuit(q,c)
+        for qubit in q:
+            qc.reset(qubit)
+
+        # initialisation
+        if(add0== '1'):
+            qc.x(q[0])
+        if(add1 == '1'):
+            qc.x(q[1])
+
+        Half_Adder(qc, q[0],q[1],q[2],c[0])
+        qc.measure(q[2], c[1])
+
+        backend = Aer.get_backend('qasm_simulator')
+        job = execute(qc, backend, shots=1000)
+        results = job.result()
+        count = results.get_counts()
+        print('|0', add0, '>', '+', '|0', add1, '>', '\t', count)
+
+## Plot a sketch of the gate
+q  = QuantumRegister(3, name = 'q')
+c  = ClassicalRegister(2, name = 'c')
+qc = QuantumCircuit(q,c)
+
+qc.reset(q[2])
+Half_Adder(qc, q[0],q[1],q[2],c[0])
+qc.measure(q[2], c[1])
+
+LaTex_code = qc.draw(output='latex_source',
+                     justify=None) # draw the circuit
+f_name = 'Half_Adder_gate_Benjamin.tex'
+with open(LaTex_folder_Adder_gates+f_name, 'w') as f:
+            f.write(LaTex_code)
+
+## Half Adder for two qubits (Beyond Classical book version)
+q  = QuantumRegister(5, name = 'q')
+c  = ClassicalRegister(2, name = 'c')
+qc = QuantumCircuit(q,c)
+addend0 = '1'
+addend1 = '0'
+# initialisation
+if(addend0 == '1'):
+    qc.x(q[0])
+if(addend1 == '1'):
+    qc.x(q[1])
+
+XOR(qc, q[0],q[1],q[2])
+qc.barrier(q)
+
+AND(qc, q[0], q[1], q[3])
+qc.barrier(q)
+
+qc.measure(q[2], c[0])
+qc.measure(q[3], c[1])
+
+backend = Aer.get_backend('qasm_simulator')
+job = execute(qc, backend, shots=1000)
+results = job.result()
+count = results.get_counts()
+#print(count)
+
+LaTex_code = qc.draw(output='latex_source', justify=None) # draw the circuit
+f_name = 'Half_Adder_gate_Beyond_Classical.tex'
+with open(LaTex_folder_Adder_gates+f_name, 'w') as f:
+            f.write(LaTex_code)
+
+## Full Adder
+addend0 = '1' 
+addend1 = '1'
+
+# ensure that the largest number is encoded in the first qubits 
+if(len(addend0)<len(addend1)):
+    help = addend0
+    addend0 = addend1
+    addend1 = help
+
+q  = QuantumRegister(len(addend0)+len(addend1)+1, name = 'q')
+c  = ClassicalRegister(len(addend0)+1, name = 'c')
+qc = QuantumCircuit(q,c)
+
+# initialisation: qubit 1
+qc.reset(q[0])
+for i, s in enumerate(addend0):
+    if(s=='1'):
+        qc.x(q[i+1])
+
+# initialisation: qubit 2
+for i, s in enumerate(addend1):
+    if(s=='1'):
+        qc.x(q[i+1+len(addend0)])
+
+# add with carry bit by bit
+for i in range(len(addend1)):
+    print(len(addend0)+len(addend1)-i,'\t',len(addend0)-i,'\t',len(addend0)-1-i)
+    Add_with_carry( qc, 
+                    q[len(addend0)+len(addend1)-i], 
+                    q[len(addend0)-i],
+                    q[len(addend0)-1-i])
+
+# read result
+for i in range(len(addend0)+1):
+    qc.measure(q[i], c[i])
+
+#backend = Aer.get_backend('qasm_simulator')
+#job = execute(qc, backend, shots=1000)
+#results = job.result()
+#count = results.get_counts()
+#print(count)
+
+LaTex_code = qc.draw(output='latex_source', justify=None) # draw the circuit
+f_name = 'Full_Adder_gate_Benjamin.tex'
+with open(LaTex_folder_Adder_gates+f_name, 'w') as f:
+            f.write(LaTex_code)
