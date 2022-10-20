117e1b24cad09579d6a26f108295cfe935321661
benkoehlL <feynmandiagramateur@gmail.com>
2022-10-20 09:03:25 +0200

    finished implementation of BB84 protocol

--- BB84.py
+++ BB84.py
@@ -45,7 +45,7 @@ def SendState(qc1, qc2, qc1_name):
 
 
 # declare classical and quantum register
-n =16 # for local backend 'n' can go up to 23, 
+n = 16 # for local backend 'n' can go up to 23, 
       #after which a memery error is raised
 qr = QuantumRegister(n, name='q')
 cr = ClassicalRegister(n, name='c')
@@ -91,9 +91,9 @@ backend = BasicAer.get_backend('qasm_simulator')
 
 # Bob has only one chance of measuring correctly
 result = execute(bob, backend=backend, shots=1).result()
-plot_histogram(result.get_counts(bob))
-draw()
-show(block=True)
+#plot_histogram(result.get_counts(bob))
+#draw()
+#show(block=True)
 
 # result of the measurement is Bob's key candidate
 bob_key = list(result.get_counts(bob))[0]
@@ -105,6 +105,7 @@ bob_key = bob_key[::-1]
 keep = []
 discard = []
 
+print('\n', "Compare Bob's and Alice's basis (without eavesdropping): ")
 for qubit, basis in enumerate(zip(alice_table,bob_table)):
     if(basis[0] == basis[1]):
         print("Same choice for qubit: {}, basis: {}".format(qubit, basis[0]))
@@ -136,6 +137,109 @@ if(acc//len(new_alice_key) == 1):
     print("New Alice's key: ", new_alice_key)
     print("New Bob's key: ", new_bob_key)
 else:
-    print('Key exchange has been tampered! Check for eavesdropper or try again')
+    print('Key exchange has been tampered! \
+        Check for eavesdropper or try again')
     print("New Alice's key is invalid: ", new_alice_key)
     print("New Bob's key is invalid: ", new_bob_key)
+
+# let's intrude a eavesdropper Eve (which is initialised to Alice's state)
+eve = QuantumCircuit(qr, cr, name='Eve')
+SendState(alice, eve, 'Alice')
+
+eve_table = []
+for qubit in qr:
+    if(np.random.rand()>0.5):
+        eve.h(qubit)
+        eve_table.append('H')
+    else:
+        eve_table.append('Z')
+for i, qubit in enumerate(qr):
+    eve.measure(qubit,cr[i])
+
+# Execute (build and run) the quantum circuit
+backend = BasicAer.get_backend('qasm_simulator')
+result = execute(eve, backend=backend, shots=1).result()
+
+# Result of the measurement is Eve's key
+eve_key = list(result.get_counts(eve))[0]
+eve_key = eve_key[::-1]
+
+# Update states to new eigenstates (of wrongly chosen basis)
+print('\n', "Compare Eve's and Alice's basis: ")
+for i, basis in enumerate(zip(alice_table,eve_table)):
+    if(basis[0] == basis[1]):
+        print("Same choice for qubit: {}, basis: {}".format(qubit, basis[0]))
+        keep.append(i)
+    else:
+        print("Different choice for qubit: {}, Alice has {}, Eve has {}".format(qubit, basis[0], basis[1]))
+        discard.append(i)
+    if eve_key[i] == alice_key[i]:
+        eve.h(qr[i])
+    else:
+        if (basis[0] == 'H' and basis[1] == 'Z'):
+            alice.h(qr[i])
+            eve.x(qr[i])
+        else:
+            eve.x(qr[i])
+            eve.h(qr[i])
+
+# Eve's state is now sent to Bob
+SendState(eve, bob, 'Eve')
+bob_table = []
+for qubit in qr:
+    if(np.random.rand()>0.5):
+        bob.h(qubit)
+        bob_table.append('H')
+    else:
+        bob_table.append('Z')
+for i, qubit in enumerate(qr):
+    bob.measure(qubit, cr[i])
+result = execute(bob, backend, shots=1).result()
+#plot_histogram(result.get_counts(bob))
+#draw()
+#show(block=True)
+
+bob_key = list(result.get_counts(bob))[0]
+bob_key = bob_key[::-1]
+
+# Now Alice and Bob will share their table data and perform checking operations
+keep = []
+discard = []
+
+print('\n', "Compare Bob's and Alice's basis (with eavesdropping by Eve): ")
+for qubit, basis in enumerate(zip(alice_table, bob_table)):
+    if(basis[0] == basis[1]):
+        print("Same choice for qubit: {}, basis: {}".format(qubit, basis[0]))
+        keep.append(qubit)
+    else:
+        print("Different choice for qubit: {}, Alice has {}, Bob has {}".format(qubit, basis[0], basis[1]))
+        discard.append(qubit)
+
+# measure the percentage of qubits to be discarded
+acc = 0
+for bit in zip(alice_key, bob_key):
+    if(bit[0]==bit[1]):
+        acc += 1
+print('Percentage of qubits to be discarded according to table comparison: ',
+        len(keep)/n)
+print('Measurement convergence by additional chance: ', acc/n)
+
+new_alice_key = [alice_key[qubit] for qubit in keep]
+new_bob_key = [bob_key[qubit] for qubit in keep]
+
+acc = 0
+for bit in zip(new_alice_key, new_bob_key):
+    if(bit[0] == bit[1]):
+        acc += 1
+print('\n Percentage of similarity between the keys: ', acc/len(new_alice_key))
+
+if(acc//len(new_alice_key) == 1):
+    print('Key exchange has been succesfull')
+    print("New Alice's key: ", new_alice_key)
+    print("New Bob's key: ", new_bob_key)
+else:
+    print('Key exchange has been tampered!', '\n',\
+        'Check for eavesdropper or try again')
+    print("New Alice's key is invalid: ", new_alice_key)
+    print("New Bob's key is invalid: ", new_bob_key)
+
