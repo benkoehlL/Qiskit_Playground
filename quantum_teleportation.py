29a67c95467da7654c94352d8cd65566277d6537
benkoehlL <feynmandiagramateur@gmail.com>
2024-02-28 12:20:49 +0100

    implemented use of c_if gates for consistency with literature

--- quantum_teleportation.py
+++ quantum_teleportation.py
@@ -4,7 +4,7 @@ This is a implementation of the quantum teleportation algorithm
 
 from qiskit import *
 from qiskit.visualization import plot_histogram
-import os, shutil
+import os, shutil, numpy
 from matplotlib.pyplot import plot, draw, show
 
 LaTex_folder_Quantum_Teleportation = str(os.getcwd())+'/Latex_quantum_gates/Quantum_Teleportation/'
@@ -15,19 +15,25 @@ else:
     os.makedirs(LaTex_folder_Quantum_Teleportation)
 
 qc = QuantumCircuit(3,3)
-# teleport |1> use -> qc.x(0)
-# teleport |0> + |1> use -> qc.x(0)
+## prepare the state to be teleported
+phi = 0*numpy.pi
+theta= 0.5*numpy.pi
+lam = 0*numpy.pi
+qc.u(phi=phi, theta=theta,lam=lam,qubit=0)
 
+## teleport the state
 qc.barrier()
 qc.h(1)
 qc.cx(1,2)
-qc.cx(0,1)
+qc.cz(0,1)
 qc.h(0)
+qc.h(1)
 qc.barrier()
 qc.measure([0,1],[0,1])
 qc.barrier()
-qc.cx(1,2)
-qc.cz(0,2)
+qc.x(2).c_if(0,1)
+qc.z(2).c_if(1,1)
+qc.h(2)
 qc.measure(2,2)
 
 LaTex_code = qc.draw(output='latex_source', initial_state=True, justify=None) # draw the circuit
@@ -38,7 +44,14 @@ with open(LaTex_folder_Quantum_Teleportation+f_name, 'w') as f:
 # simulation
 simulator = Aer.get_backend('qasm_simulator')
 result = execute(qc, backend=simulator, shots=100000).result()
-counts = result.get_counts()
+counts = {'0':0,
+           '1': 0}
+print(result.get_counts().keys())
+for key, value in result.get_counts().items():
+    if(key[0] == '0'):
+          counts['0'] += value
+    else:
+          counts['1'] += value
 print(counts)
 plt = plot_histogram(counts)
 draw()
